b2c731e1fd7bd97a6d2735ae138acedb2643987d
DevNakum <ndev2003@gmail.com>
2022-03-22 21:41:29 +0530

    initial commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+    $hostname = "http://localhost/PHP/Project/Library-System";
+    $conn = mysqli_connect("localhost","root","","library") or die("Connection failed : ".mysqli_connect_error());
+?>
