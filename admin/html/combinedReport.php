a6ea336f70adac607ad915b235ab70499bc11e04
DevNakum <ndev2003@gmail.com>
2022-03-22 21:45:23 +0530

    initial commit

---
+++ admin/html/combinedReport.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    />
+  <link rel="stylesheet" href="../css/combinedReport.css">
+</head>
+
+<body>
+  <header>
+    Library Helper - My Library
+  </header>
+  <nav>
+    <input type="checkbox" id="check" />
+    <label for="check" class="checkbtn">
+      <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo">ID No.</label>
+    <ul>
+      <li><a href="Admin_Profile.php">Home</a></li>
+            <li><a class="active" href="#">Combine Report</a></li>
+            <li><a href="#">Credit</a></li>
+            <li><a href="../../about_us.php">About us</a></li>
+            <li><a href="#">Contact us</a></li>
+    </ul>
+  </nav>
+    <div class="search">
+            <input type="text" placeholder=" Enter ID.." name="txtSearch" class="txtSearch" autocomplete="off">   
+            <button type="submit" class="btnSearch">Search</button>    
+            <button class="btnDownload">Download</button>      
+    </div>
+    
+    
+
+  <div class="tblReturnReport" style="overflow-x:auto;">
+    <table>
+      <tr>
+        <th>Book Name</th>
+        <th>Edition</th>
+        <th>Author</th>
+        <th>Total copies</th>
+        <th>Issued copies</th>
+      </tr>
+      <tr>
+        <td>BALAGURU SWAMI</td>
+        <td>2nd</td>
+        <td>BALAGURU</td>
+        <td>10</td>
+        <td>8</td>
+      </tr>
+      <tr>
+        <td>BALAGURU SWAMI</td>
+        <td>2nd</td>
+        <td>BALAGURU</td>
+        <td>10</td>
+        <td>8</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+
+</body>
+
+</html>
---
+++ admin/html/setRecommandation.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="../css/setRecommandation.css">
+</head>
+
+<body>
+    <header>
+        Library Helper - My Library
+    </header>
+    <nav>
+        <input type="checkbox" id="check" />
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">ID No.</label>
+        <ul>
+            <li><a href="Admin_Profile.php">Home</a></li>
+            <li><a href="combinedReport.php">Combine Report</a></li>
+            <li><a class="active" href="#">Credit</a></li>
+            <li><a href="../../about_us.php">About us</a></li>
+            <li><a href="#">Contact us</a></li>
+        </ul>
+    </nav>
+    <div class="search">
+        <select name="dept" id="optDept" style="border-style: solid;">
+
+            <option class="optDept" value="select" name="select">---Select---</option>
+            <option class="optDept" value="CSE" name="CSE">CSE</option>
+            <option class="optDept" value="CE" name="CE">CE</option>
+            <option class="optDept" value="IT" name="IT">IT</option>
+            <option class="optDept" value="ME" name="ME">ME</option>
+            <option class="optDept" value="Civil Eng." name="CivilEng">Civil Engineering</option>
+
+        </select>
+        <select name="sem" id="optSem" style="border-style: solid;">
+            <option class="optSem" value="select" name="select">---Select---</option>
+            <option class="optSem" value="sem1" name="sem1">sem1</option>
+            <option class="optSem" value="sem2" name="sem2">sem2</option>
+            <option class="optSem" value="sem3" name="sem3">sem3</option>
+            <option class="optSem" value="sem4" name="sem4">sem4</option>
+            <option class="optSem" value="sem5" name="sem5">sem5</option>
+            <option class="optSem" value="sem6" name="sem6">sem6</option>
+            <option class="optSem" value="sem7" name="sem7">sem7</option>
+            <option class="optSem" value="sem8" name="sem8">sem8</option>
+        </select>
+        <button class="btnDownload" name="btnDownload">Go</button>
+    </div>
+
+
+
+    <div class="tblReturnReport" style="overflow-x:auto;">
+        <table name="table">
+            <tr>
+                <th></th>
+                <th>Book Name</th>
+            </tr>
+            <tr>
+                <td>
+                    <label class="switch">
+                        <input type="checkbox" name="chkSubject">
+                        <span class="slider round"></span>
+                    </label>
+                </td>
+                <td>MCO</td>
+            </tr>
+            <tr>
+                <td>
+                    <label class="switch">
+                        <input type="checkbox" name="chkSubject">
+                        <span class="slider round"></span>
+                    </label>
+                </td>
+                <td>OOPC</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+    <div class="submit">
+        <button class="btnSubmit">Submit</button>
+    </div>
+
+</body>
+
+</html>
