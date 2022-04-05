407852e58186b335fd3c6c23a9edaf5b24e214a6
DevNakum <ndev2003@gmail.com>
2022-04-05 19:04:04 +0530

    hostname changes

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
     $hostname = "http://localhost/PHP/Project/Clg-Project/MyLibrary-LibraryHelper";
+    // $hostname = "http://localhost/Library-Helper";
     $conn = mysqli_connect("localhost","root","","library") or die("Connection failed : ".mysqli_connect_error());
 ?>
