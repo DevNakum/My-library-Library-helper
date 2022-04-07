7efbb1bd7fc918a95d76b4b48cc9dfb26fed25ae
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-04-07 17:23:09 +0530

    a little changed in login script.

--- user/html/login.php
+++ user/html/login.php
@@ -1,5 +1,5 @@
 <?php
-    echo "On login page!";
+    // echo "On login page!";
     if (isset($_POST['btnLogin'])) {
         include 'config.php';
        
@@ -18,9 +18,17 @@
                 $_SESSION["user_name"] = $row['user_name'];
                 $_SESSION["user_id"] = $row['user_id'];
                 $_SESSION["user_role"] = $row['user_role'];
-                // echo "{$hostname}/user/html/userProfile.php";
-                header("Location: {$hostname}/user/html/userProfile.php");
+                
+                // according to role, we redirect on the pages...
+                // how about that ?...
+                if($_SESSION['user_role']==1) {
+                    header("Location: {$hostname}/admin/html/Admin_Profile.php");
+                } else {
+                    header("Location: {$hostname}/user/html/userProfile.php");
+                }
             }
+        } else {
+            echo "Wrong password or email, please check the credentials again!";
         }
     }
 ?>
